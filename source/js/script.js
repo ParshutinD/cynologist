8fb20aaf8a7278edf039035e192bca90e35f0786
unknown <daniilparshutin1@gmail.com>
2022-12-21 08:14:36 +0300

    доработки

--- source/js/script.js
+++ source/js/script.js
@@ -21,3 +21,37 @@ burger.addEventListener("click", function () {
   burgerMenu.classList.toggle("active");
   body.classList.toggle("no-scroll");
 });
+
+
+// scroll
+const smoothLinks = document.querySelectorAll('a[href^="#"]');
+for (let smoothLink of smoothLinks) {
+    smoothLink.addEventListener('click', function (e) {
+        e.preventDefault();
+        const id = smoothLink.getAttribute('href');
+
+        document.querySelector(id).scrollIntoView({
+            behavior: 'smooth',
+            block: 'start'
+        });
+    });
+};
+
+
+
+function onEntry(entry) {
+  entry.forEach(change => {
+    if (change.isIntersecting) {
+     change.target.classList.add('element-show');
+    }
+  });
+}
+
+let options = {
+  threshold: [0.5] };
+let observer = new IntersectionObserver(onEntry, options);
+let elements = document.querySelectorAll('.element-animation');
+
+for (let elm of elements) {
+  observer.observe(elm);
+}
